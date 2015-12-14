d476b20305822b617f077b94bb3cb8066b3a7884
Sarah Cohen <sarahrose.cohen@gmail.com>
2015-12-14 15:07:21 -0800

    let's try this again

--- hash_exp.rb
+++ hash_exp.rb
@@ -3,3 +3,4 @@ my_hash = {name: "Sarah", phone: 5102953295, hair_color: "dirty blond", body_typ
 puts my_hash[:name]
 
 puts "this is a test"
+puts "this is another test"
--- test.rb
+++
@@ -1,1 +0,0 @@
-puts "this is also a test"
